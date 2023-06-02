da678eeb2185bfd2fd43530b9b41b068939b2250
Tejas Bhosale <50192323+tejasbhosale17@users.noreply.github.com>
2023-06-02 09:33:58 +0530

    java codes

---
+++ hii_in_hii_out.java
@@ -0,0 +1,12 @@
+package com.sunbeam;
+
+import java.util.Scanner;
+
+public class hii_in_hii_out {
+public static void main(String[] args) {
+	Scanner scan = new Scanner(System.in);
+	System.out.println("Enter your name:");
+	String name= scan.next();
+	System.out.println("my name is "+name);
+}
+}
